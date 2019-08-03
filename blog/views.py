309cb5da112beeb897cd7fb6afbf9307afed6234
Siri Chandana <siri.goparaju@gmail.com>
2019-08-03 13:40:14 -0500

    Made changes to the Post_list for tone

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 import json
-# from ibm_watson import ToneAnalyzerV3
+#from ibm_watson import ToneAnalyzerV3
 
 import requests
 from watson_developer_cloud import ToneAnalyzerV3
@@ -33,18 +33,39 @@ def post_list(request):
         translation = language_translator.translate(
             text= post.text, model_id='en-es').get_result()
         obj = (json.dumps(translation, indent=2, ensure_ascii=False))
-        print(obj)
+        # print(obj)
         obj2 = json.loads(obj)
         post.obj2 = obj2['translations'][0]['translation']
         post.w_count = obj2['word_count']
         post.c_count = obj2['character_count']
 
         tone_input = ToneInput(post.text)
-        tone = tone_analyzer.tone(tone_input=tone_input, content_type="application/json")
-        tone2 = str(tone)
-        post.tone3 = (tone2[1:500])
-        print(post.tone3)
+        # tone = tone_analyzer.tone(tone_input=tone_input, content_type="application/json")
+        # print(tone)
+        # print('//////////////////////////////////////////////////////')
+        # tone2 = str(tone)
+        # post.tone3 = (tone2[1:500])
+        # print('*********************************************************************')
+        # print(post.tone3)
+        # print('*********************************************************************')
+        # JsonText = json.dumps(post.tone3)
+        # d2=json.loads(JsonText)
+        # print(d2['result'][0])
 
+        # jsonText = json.dumps(tone, indent=2, ensure_ascii=False)
+        # tone_score = json.loads(jsonText)['results']
+        # print(tone_score)
+        try:
+            tone = tone_analyzer.tone(tone_input=tone_input, content_type="application/json").get_result()
+            jsonText = (json.dumps(tone, indent=2, ensure_ascii=False))
+            jsonParse = json.loads(jsonText)
+            post.Score1 = jsonParse['document_tone']['tones'][0]['score']
+            post.ToneName1 = jsonParse['document_tone']['tones'][0]['tone_name']
+        except:
+            pass
+            post.Score1 = "no-data value for key Score from API response"
+            post.ToneName1 ="no-data value for key ToneName from API response"
+            print("****************************Something went wrong while rendering the Document tone json")
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
